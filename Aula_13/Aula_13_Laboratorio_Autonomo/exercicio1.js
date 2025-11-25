c9fd71b8b3e037f5eec709cadce692fec8a7c38a
Danilson Jorge Tavares Sanches <upskill217@alunos.fc.ul.pt>
2025-11-25 13:01:07 +0000

    feat: Resolução dos exercicios 1 e 2 do Laboratorio 13, utilizando classes en javascript

--- Aula_13/Aula_13_Laboratorio_Autonomo/exercicio1.js
+++ Aula_13/Aula_13_Laboratorio_Autonomo/exercicio1.js
@@ -1,69 +1,76 @@
-
 /* Exercício 1 — Sistema Bancário Complexo */
-console.log("Exercício 1 — Sistema Bancário Complexo");
+console.log("\n--- Exercício 1 — Sistema Bancário Complexo ---");
 
 //classe/objeto Conta
-const conta = {
-  saldo: 0,
+class Conta {
+  constructor(saldo) {
+    this.saldo = saldo;
+  }
   depositar(valor) {
     //adiciona valor ao saldo; mostrar saldo.
     this.saldo += valor;
     this.mostrarSaldo();
-  },
+  }
   levantar(valor) {
     //subtrai valor do saldo se houver saldo; mostrar saldo.
     if (this.saldo >= valor) {
       this.saldo -= valor;
       this.mostrarSaldo();
     }
-  },
+  }
   aplicarJuros(taxa, anos) {
     //ciclo for para aplicar juros ano a ano; mostrar saldo em cada ano.
-    //let saldoAtual = this.saldo;
     for (let i = 1; i <= anos; i++) {
-      this.saldo *= i + taxa;
-      console.log(`Ano: ${i} - Saldo: ${this.saldo.toFixed(2)}`);
+      this.saldo += this.saldo * taxa;
+      console.log(`- ANO ${i} -`);
+      this.mostrarSaldo();
     }
-  },
+  }
   mostrarSaldo() {
     //→ imprime saldo atual.
-    console.log(`Saldo Atual: ${this.saldo.toFixed(2)}\n`);
-  },
-};
+    console.log(`SALDO ATUAL: ${this.saldo.toFixed(2)}`);
+  }
+}
 
 //classe/objeto Cliente
-const cliente = {
-  identificador: "Cliente 1",
-  contaAtual: conta,
+class Cliente {
+  constructor(identificador, conta) {
+    this.identificador = identificador;
+    this.conta = conta;
+  }
+
   transferir(valor, outraConta) {
-    //retira de sua conta e adiciona à outra.
-    this.contaAtual.levantar(valor);
-    outraConta.depositar(valor);
-  },
+    //retira de sua conta se houver saldo e adiciona à outra.
+    if (valor <= this.conta.saldo) {
+      this.conta.levantar(valor);
+      outraConta.depositar(valor);
+    } else {
+      console.log("ERRO: Saldo menor que o valor!");
+    }
+  }
   mostrarSaldo() {
     //mostra saldo da conta.
-    console.log(`--- Saldo de ${this.identificador} ---`);
-    this.contaAtual.mostrarSaldo();
-  },
+    this.conta.mostrarSaldo();
+  }
   aniversario(anos, bonus) {
     //bônus de saldo anual; use ciclo for para anos passados.
     for (let i = 1; i <= anos; i++) {
-      let valorBonusAnual = this.contaAtual.saldo * bonus;
-      this.contaAtual.saldo += valorBonusAnual;
+      this.conta.saldo += this.conta.saldo * bonus;
     }
-  },
+  }
   presentear(valor, outroCliente) {
     //transfere valor a outro cliente.
-    outroCliente.depositar(valor);
-  },
-};
-
-
+    this.transferir(valor, outroCliente.conta);
+  }
+}
 
 //classe/objeto Banco
-const banco = {
-  saldoBancoTotal: 1000000,
-  jurosDiario: 0.01,
+class Banco {
+  constructor() {
+    this.saldo = 100000;
+    this.jurosDiario = 0.01;
+  }
+
   simularDia(cliente, dia) {
     //aplica juros diários em cada cliente usando ciclo.
     for (let i = 1; i <= dia; i++) {
@@ -76,8 +83,8 @@ const banco = {
         console.log(
           `Com juros diario de ${this.jurosDiario} fica ${valor.toFixed(2)}`
         );
-        cliente.contaAtual.levantar(valor);
-        this.saldoBancoTotal += valor * this.jurosDiario;
+        cliente.conta.levantar(valor);
+        this.saldo += valor * this.jurosDiario;
       } else {
         //deposita e paga juros diario
         console.log(`Dia ${i} - Depositar ${valor.toFixed(2)}`);
@@ -86,85 +93,136 @@ const banco = {
         console.log(
           `Com juros diario de ${this.jurosDiario} fica ${valor.toFixed(2)}`
         );
-        cliente.contaAtual.depositar(valor);
-        this.saldoBancoTotal += valor * this.jurosDiario;
+        cliente.conta.depositar(valor);
+        this.saldo += valor * this.jurosDiario;
       }
     }
     this.resumoTotal();
-  },
+  }
   relatorio(cliente) {
     //imprime saldo de todos os clientes.
-   console.log(`Saldo de ${cliente.identificador}: ${cliente.mostrarSaldo()}`);
-  },
+    cliente.mostrarSaldo();
+  }
   aplicarTaxaDia(taxa, cliente) {
     //aplica taxa diária a todas contas
     //  cliente;
-  },
+  }
   resumoTotal() {
-    console.log(`Saldo total do Banco: ${this.saldoBancoTotal.toFixed(2)}.`);
-  },
-};
+    //soma e mostra saldo total do banco
+    console.log(`\nBANCO - SALDO TOTAL: ${this.saldo.toFixed(2)}.`);
+  }
+}
+
+//CRIAÇÃO DE OBJETOS
+const conta1 = new Conta(2580);
+const cliente1 = new Cliente(1, conta1);
+const conta2 = new Conta(1320);
+const cliente2 = new Cliente(2, conta2);
+const banco = new Banco();
+
+//conta
+console.log("\n--- MOSTRAR SALDO DA CONTA 1 ---");
+conta1.mostrarSaldo();
+console.log("\n--- DEPOSITAR ---");
+conta1.depositar(470);
+console.log("\n--- LEVANTAR---");
+conta1.levantar(100);
+console.log("\n--- APLICAR JUROS ---");
+conta1.aplicarJuros(0.02, 5);
+
+console.log("\n--- MOSTRAR SALDO DA CONTA 2 ---");
+conta1.mostrarSaldo();
+console.log("\n--- DEPOSITAR ---");
+conta1.depositar(260);
+console.log("\n--- LEVANTAR---");
+conta1.levantar(420);
+console.log("\n--- APLICAR JUROS ---");
+conta1.aplicarJuros(0.02, 3);
+
+//cliente
+console.log("\nTESTE CLIENTE 1");
+cliente1.mostrarSaldo();
+console.log("\n--- BONUS ANIVERSARIO ---");
+cliente1.aniversario(1, 0.02);
+cliente1.mostrarSaldo();
+
+console.log("\nTESTE CLIENTE 2");
+cliente2.mostrarSaldo();
+console.log("\n--- BONUS ANIVERSARIO ---");
+cliente1.aniversario(1, 0.02);
+cliente2.mostrarSaldo();
+
+console.log("\nCLIENTE 1 - PRESENTEAR CLIENTE 2");
+cliente1.presentear(150, cliente2);
+
+console.log("\nSALDO DO CLIENTE 2 DEPOIS DE SER PRESENTEADO PELO CLIENTE 2");
+cliente2.mostrarSaldo();
+
+console.log("\nCLIENTE 2 - PRESENTEAR CLIENTE 1");
+cliente2.presentear(350, cliente1);
+
+console.log("\nSALDO DO CLIENTE 1 DEPOIS DE SER PRESENTEADO PELO CLIENTE 2");
+cliente1.mostrarSaldo();
+
+//Banco
+console.log("\n--- TESTE BANCO ---");
+banco.resumoTotal();
+
+console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 1");
+banco.relatorio(cliente1);
+console.log("\nBANCO - SIMULAÇÃO DIARA DO CLIENTE 1");
+banco.simularDia(cliente1, 7);
+console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 1");
+banco.relatorio(cliente2);
+
+console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 2");
+banco.relatorio(cliente2);
+console.log("\nBANCO - SIMULAÇÃO DIARA DO CLIENTE 2");
+banco.simularDia(cliente2, 7);
+console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 2");
+banco.relatorio(cliente2);
 
 //classe/objeto Gerente
-const gerente = {
+class Gerente {
+  constructor() {}
   auditar(cliente) {
     // imprime histórico e saldo da conta.
     console.log(`--- Histórico de ${cliente.identificador} ---`);
     cliente.mostrarSaldo();
-  },
+  }
   bonificar(cliente, valor) {
     // adiciona bônus ao saldo do cliente.
     console.log(`Bonificar ${cliente.identificador} com valor de ${valor}`);
     cliente.contaAtual.depositar(valor);
     cliente.mostrarSaldo();
-  },
+  }
   reduzirSalario(cliente, valor) {
     // subtrai valor com segurança.
-    console.log(`Reduzir o salario do ${cliente.identificador} com valor de ${valor}`);
+    console.log(
+      `Reduzir o salario do ${cliente.identificador} com valor de ${valor}`
+    );
     cliente.contaAtual.levantar(valor);
-  },
+  }
   mostrarResumo(banco) {
     // mostra resumo do banco e clientes
     banco.resumoTotal();
-  },
-};
+  }
+}
 
 //classe/objeto CaixaEletronico
-const caixaEletronico = {
+class CaixaEletronico {
+  constructor() {}
   sacar(cliente, valor) {
     cliente.contaAtual.levantar(valor);
-  },
+  }
   depositar(cliente, valor) {
     cliente.contaAtual.depositar(valor);
-  },
+  }
   mostrarSaldo(cliente) {
     cliente.mostrarSaldo();
-  },
+  }
   simularTransaccoes(banco, cliente, dias) {
     //  simular transações diárias.
     banco.simularDia(cliente, dias);
-  },
-};
-
-console.log("\n--- CLIENTE ---");
-console.log("Deposito de 100 EUR...");
-cliente.contaAtual.depositar(100);
-console.log("Saque de 50 EUR...");
-cliente.contaAtual.levantar(50);
-console.log("Aplicar 3% de juros por 5 anos...");
-cliente.contaAtual.aplicarJuros(0.03, 5);
-cliente.contaAtual.mostrarSaldo();
-console.log("Bonus de 5% por cada aniversario...")
-cliente.aniversario(5, 0.05);
-cliente.mostrarSaldo();
-
-console.log("-- GERENTE ---"); 
-gerente.auditar(cliente);
-gerente.bonificar(cliente, 100);
-gerente.reduzirSalario(cliente, 50);
-console.log("\n--- MOSTRAR RESUMO ---");
-gerente.mostrarResumo(banco);
-
-console.log("\n--- CAIXA ELETRONICO - SIMULANDO 30 DIAS ---");
-caixaEletronico.simularTransaccoes(banco,cliente, 30);
-
+  }
+}
--- Aula_13/Aula_13_Laboratorio_Autonomo/exercicio2.js
+++ Aula_13/Aula_13_Laboratorio_Autonomo/exercicio2.js
@@ -1,81 +1,105 @@
-const { corrida, piloto, carro } = require("./exercicio3");
-
 /* Exercício 2 — Escola e Provas */
 console.log("\nExercício 2 — Escola e Provas");
 
-const aluno = {
-  notaFinal: 0,
+class Aluno {
+  constructor() {
+    this.notaFinal = 0;
+  }
   realizarProva(prova) {
     //ciclo para responder todas as questões; atribuir pontos aleatórios.
-  },
+    this.notaFinal += prova.aplicarQuestao();
+    console.log("Nota Final: ", this.notaFinal);
+  }
   calcularMedia() {
     //soma notas e divide por quantidade de provas simuladas.
-  },
+  }
   estudar(horas) {
     //incrementa notaFinal baseado em horas estudadas.
-  },
+    for (let i = 1; i <= horas; i++) {
+      this.notaFinal += i;
+    }
+  }
   receberBonus(pontos) {
-    //adiciona pontos extras à notaFinal.
-  },
-};
+    //adiciona pontos extras à notaFinal
+    this.notaFinal += pontos;
+  }
+}
 
-const prova = {
-  aplicarQuestao(aluno) {
+class Prova {
+  constructor() {
+    this.questoes = 10;
+  }
+  aplicarQuestao() {
     //ciclo para cada questão; gera nota aleatória.
-  },
+    let notaTotal = 0;
+    for (let i = 1; i <= this.questoes; i++) {
+      //gera a nota
+      let notaAleatoria = Math.floor(Math.random() * 3);
+      console.log("Nota aleatoria: ", notaAleatoria);
+      notaTotal += notaAleatoria;
+      console.log(`Questão ${i} - Nota: ${notaTotal}`);
+    }
+    return notaTotal;
+  }
   corrigir() {
     //ajusta notas baseadas em respostas.
-  },
+  }
   mostrarResultado() {
     //imprime resultado do aluno.
-  },
+  }
   simular() {
     //ciclo para aplicar prova a múltiplos alunos.
-  },
-};
+  }
+}
 
-const professor = {
-  aplicarProva(aluno, prova) {
+const aluno1 = new Aluno();
+const prova1 = new Prova();
+aluno1.realizarProva(prova1);
+
+class Professor {
+  constructor() {}
+  aplicarProva(aluno) {
     //chama método do aluno para responder prova.
-  },
+  }
   corrigirProva(aluno, prova) {
     //corrige usando métodos da prova.
-  },
+  }
   darBonus(aluno) {
     //incrementa pontos extra.
-  },
+  }
   mostrarNotas(alunos) {
     // imprime notaFinal do aluno.
-  },
-};
+  }
+}
 
-const disciplina = {
+class Disciplina {
+  constructor() {}
   iniciarAula() {
     // ciclo para todos os alunos participarem.
-  },
+  }
   aplicarProvas() {
     // chama métodos de aplicarProva do professor.
-  },
+  }
   resumo() {
     //mostra todas notas.
-  },
+  }
   ranking() {
     //compara notas de alunos e mostra ordem.
-  },
-};
+  }
+}
 
-const secretaria = {
+class Secretaria {
+  constructor() {}
   registarAluno(aluno) {
     //adiciona aluno ao sistema
-  },
+  }
   gerarBoletim(aluno) {
     //imprime boletim.
-  },
+  }
   simularDia() {
     //ciclo para todas atividades escolares.
-  },
+  }
   atualizarNotas(aluno) {
     //ajusta notaFinal conforme regras.
-  },
-};
-
+  }
+}
