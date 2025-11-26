64dee93f42c18ebdc1421b93678968089731e146
Danilson Jorge Tavares Sanches <upskill217@alunos.fc.ul.pt>
2025-11-26 11:32:54 +0000

    feat: Adicionar e corrigir alguns metodos no exercicio 1 do laboratorio da Aula 13

--- Aula_13/Aula_13_Laboratorio_Autonomo/exercicio1.js
+++ Aula_13/Aula_13_Laboratorio_Autonomo/exercicio1.js
@@ -5,16 +5,21 @@ console.log("\n--- Exercício 1 — Sistema Bancário Complexo ---");
 class Conta {
   constructor(saldo) {
     this.saldo = saldo;
+    this.historicos = [];
   }
   depositar(valor) {
     //adiciona valor ao saldo; mostrar saldo.
     this.saldo += valor;
+    const operacao = { tipo: "DEPOSITO", valor };
+    this.historicos.push(operacao);
     this.mostrarSaldo();
   }
   levantar(valor) {
     //subtrai valor do saldo se houver saldo; mostrar saldo.
     if (this.saldo >= valor) {
       this.saldo -= valor;
+      const operacao = { tipo: "SAQUE", valor };
+      this.historicos.push(operacao);
       this.mostrarSaldo();
     }
   }
@@ -69,6 +74,7 @@ class Banco {
   constructor() {
     this.saldo = 100000;
     this.jurosDiario = 0.01;
+    this.transaccoes = [];
   }
 
   simularDia(cliente, dia) {
@@ -76,6 +82,9 @@ class Banco {
     for (let i = 1; i <= dia; i++) {
       let valor = Math.floor(Math.random() * 1000);
       if (Math.random() < 0.5) {
+        //salva o tipo de transacao diaria
+        let transacao = { dia: i, tipo: "SAQUE", valor };
+        this.transaccoes.push(transacao);
         //saca e paga juros diario
         console.log(`Dia ${i} - Sacar ${valor.toFixed(2)}`);
         let valorJuros = valor * this.jurosDiario;
@@ -86,6 +95,9 @@ class Banco {
         cliente.conta.levantar(valor);
         this.saldo += valor * this.jurosDiario;
       } else {
+        //salva o tipo de transacao diaria
+        let transacao = { dia: i, tipo: "DEPOSITO", valor };
+        this.transaccoes.push(transacao);
         //deposita e paga juros diario
         console.log(`Dia ${i} - Depositar ${valor.toFixed(2)}`);
         let valorJuros = valor * this.jurosDiario;
@@ -113,12 +125,63 @@ class Banco {
   }
 }
 
-//CRIAÇÃO DE OBJETOS
+//classe/objeto Gerente
+class Gerente {
+  constructor() {}
+  auditar(cliente) {
+    // imprime histórico e saldo da conta.
+    console.log(`--- Histórico de CLIENTE ${cliente.identificador} ---`);
+    let operacoes = cliente.conta.historicos;
+    for (let operacao of operacoes) {
+      console.log(operacao);
+    }
+    cliente.mostrarSaldo();
+  }
+  bonificar(cliente, valor) {
+    // adiciona bônus ao saldo do cliente.
+    console.log(
+      `Bonificar CLIENTE ${cliente.identificador} com valor de ${valor}`
+    );
+    cliente.conta.depositar(valor);
+    cliente.mostrarSaldo();
+  }
+  reduzirSalario(cliente, valor) {
+    // subtrai valor com segurança.
+    console.log(
+      `Reduzir o salario do CLIENTE ${cliente.identificador} com valor de ${valor}`
+    );
+    cliente.conta.levantar(valor);
+  }
+  mostrarResumo(banco) {
+    // mostra resumo do banco e clientes
+    banco.resumoTotal();
+  }
+}
+
+//classe/objeto CaixaEletronico
+class CaixaEletronico {
+  constructor() {}
+  sacar(cliente, valor) {
+    cliente.conta.levantar(valor);
+  }
+  depositar(cliente, valor) {
+    cliente.conta.depositar(valor);
+  }
+  mostrarSaldo(cliente) {
+    cliente.mostrarSaldo();
+  }
+  simularTransaccoes(banco, cliente, dias) {
+    //  simular transações diárias
+    banco.simularDia(cliente, dias);
+    for(let transacao of banco.transaccoes){
+      console.log(transacao);
+    }
+  }
+}
+
+//CRIAÇÃO DE OBJETOS DO TIPO CONTA
 const conta1 = new Conta(2580);
-const cliente1 = new Cliente(1, conta1);
 const conta2 = new Conta(1320);
-const cliente2 = new Cliente(2, conta2);
-const banco = new Banco();
 
 //conta
 console.log("\n--- MOSTRAR SALDO DA CONTA 1 ---");
@@ -129,15 +192,21 @@ console.log("\n--- LEVANTAR---");
 conta1.levantar(100);
 console.log("\n--- APLICAR JUROS ---");
 conta1.aplicarJuros(0.02, 5);
+console.log("------");
 
 console.log("\n--- MOSTRAR SALDO DA CONTA 2 ---");
-conta1.mostrarSaldo();
+conta2.mostrarSaldo();
 console.log("\n--- DEPOSITAR ---");
-conta1.depositar(260);
+conta2.depositar(260);
 console.log("\n--- LEVANTAR---");
-conta1.levantar(420);
+conta2.levantar(420);
 console.log("\n--- APLICAR JUROS ---");
-conta1.aplicarJuros(0.02, 3);
+conta2.aplicarJuros(0.02, 3);
+console.log("------");
+
+//CRIAÇÃO DE OBJETOS DO TIPO CLIENTE
+const cliente1 = new Cliente(1, conta1);
+const cliente2 = new Cliente(2, conta2);
 
 //cliente
 console.log("\nTESTE CLIENTE 1");
@@ -145,12 +214,14 @@ cliente1.mostrarSaldo();
 console.log("\n--- BONUS ANIVERSARIO ---");
 cliente1.aniversario(1, 0.02);
 cliente1.mostrarSaldo();
+console.log("------");
 
 console.log("\nTESTE CLIENTE 2");
 cliente2.mostrarSaldo();
 console.log("\n--- BONUS ANIVERSARIO ---");
 cliente1.aniversario(1, 0.02);
 cliente2.mostrarSaldo();
+console.log("------");
 
 console.log("\nCLIENTE 1 - PRESENTEAR CLIENTE 2");
 cliente1.presentear(150, cliente2);
@@ -163,6 +234,10 @@ cliente2.presentear(350, cliente1);
 
 console.log("\nSALDO DO CLIENTE 1 DEPOIS DE SER PRESENTEADO PELO CLIENTE 2");
 cliente1.mostrarSaldo();
+console.log("------");
+
+//CRIAÇÃO DE OBJETO DO TIPO BANCO
+const banco = new Banco();
 
 //Banco
 console.log("\n--- TESTE BANCO ---");
@@ -170,59 +245,63 @@ banco.resumoTotal();
 
 console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 1");
 banco.relatorio(cliente1);
-console.log("\nBANCO - SIMULAÇÃO DIARA DO CLIENTE 1");
-banco.simularDia(cliente1, 7);
 console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 1");
-banco.relatorio(cliente2);
+banco.relatorio(cliente1);
+console.log("------");
 
 console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 2");
 banco.relatorio(cliente2);
-console.log("\nBANCO - SIMULAÇÃO DIARA DO CLIENTE 2");
-banco.simularDia(cliente2, 7);
 console.log("\nBANCO - MOSTRAR RELATORIO DO CLIENTE 2");
 banco.relatorio(cliente2);
+console.log("------");
 
-//classe/objeto Gerente
-class Gerente {
-  constructor() {}
-  auditar(cliente) {
-    // imprime histórico e saldo da conta.
-    console.log(`--- Histórico de ${cliente.identificador} ---`);
-    cliente.mostrarSaldo();
-  }
-  bonificar(cliente, valor) {
-    // adiciona bônus ao saldo do cliente.
-    console.log(`Bonificar ${cliente.identificador} com valor de ${valor}`);
-    cliente.contaAtual.depositar(valor);
-    cliente.mostrarSaldo();
-  }
-  reduzirSalario(cliente, valor) {
-    // subtrai valor com segurança.
-    console.log(
-      `Reduzir o salario do ${cliente.identificador} com valor de ${valor}`
-    );
-    cliente.contaAtual.levantar(valor);
-  }
-  mostrarResumo(banco) {
-    // mostra resumo do banco e clientes
-    banco.resumoTotal();
-  }
-}
+//CRIAÇÃO DE OBJETOS DO TIPO GERENTE
+const gerente = new Gerente();
 
-//classe/objeto CaixaEletronico
-class CaixaEletronico {
-  constructor() {}
-  sacar(cliente, valor) {
-    cliente.contaAtual.levantar(valor);
-  }
-  depositar(cliente, valor) {
-    cliente.contaAtual.depositar(valor);
-  }
-  mostrarSaldo(cliente) {
-    cliente.mostrarSaldo();
-  }
-  simularTransaccoes(banco, cliente, dias) {
-    //  simular transações diárias.
-    banco.simularDia(cliente, dias);
-  }
-}
+//Gerente
+console.log("\n--- TESTE GERENTE ---");
+
+console.log("\nCLIENTE 1");
+gerente.bonificar(cliente1, 3456);
+console.log("\nCLIENTE 1");
+gerente.reduzirSalario(cliente1, 2784);
+console.log("\nCLIENTE 1");
+gerente.auditar(cliente1);
+console.log("------");
+
+console.log("\nCLIENTE 2");
+gerente.bonificar(cliente2, 3456);
+console.log("\nCLIENTE 2");
+gerente.reduzirSalario(cliente2, 2784);
+console.log("\nCLIENTE 2");
+gerente.auditar(cliente2);
+console.log("------");
+
+gerente.mostrarResumo(banco);
+console.log("------");
+
+//CRIAÇÃO DE OBJETOS DO TIPO CAIXA ELETRONICA
+const caixaEletronico = new CaixaEletronico();
+
+//Banco
+console.log("\n--- TESTE CAIXA ELETRONICA ---");
+
+console.log("\nCLIENTE 1");
+caixaEletronico.sacar(cliente1, 2500);
+console.log("\n CLIENTE 1");
+caixaEletronico.depositar(cliente1, 1024);
+console.log("\n CLIENTE 1");
+caixaEletronico.mostrarSaldo(cliente1);
+console.log("------");
+
+console.log("\nCLIENTE 2");
+caixaEletronico.sacar(cliente2, 1890);
+console.log("\n CLIENTE 2");
+caixaEletronico.depositar(cliente2, 3645);
+console.log("\n CLIENTE 2");
+caixaEletronico.mostrarSaldo(cliente2);
+
+console.log("\n SIMULAR TRANSAÇÕES ");
+caixaEletronico.simularTransaccoes(banco, cliente1, 30);
+caixaEletronico.simularTransaccoes(banco, cliente2, 30);
+console.log("------");
